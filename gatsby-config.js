19a8e6c7bac8e2ceec4ba108335cf20cf2d0cd84
Brittney <brittney.postma@hotmail.com>
2020-06-14 11:33:04 -0400

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Cici Cricut Creations`,
+    description: `A variety of Cricut designs printed to t-shirts, glasses, cards, and more! Cici's Cricut Creations, *Cici Cricut Creations*, Creations by Cici, Cici's Creations, Cici Creations`,
+    author: `@CiciCricutCreations`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,6 +27,7 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-stripe`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
