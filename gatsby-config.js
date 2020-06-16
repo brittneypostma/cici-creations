1aba10c44ecd006b684639278d95bce62dbe1cfa
Brittney <brittney.postma@hotmail.com>
2020-06-15 23:10:58 -0400

    query broken stripe needs sku

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,23 @@
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Cici Cricut Creations`,
-    description: `A variety of Cricut designs printed to t-shirts, glasses, cards, and more! Cici's Cricut Creations, *Cici Cricut Creations*, Creations by Cici, Cici's Creations, Cici Creations`,
-    author: `@CiciCricutCreations`,
+    title: `Cici Creations`,
+    description: `Cici Creations is your shop for cricut designs, signs, printouts, t-shirts, and more.`,
+    author: `@CiciCreations`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-stripe`,
+      options: {
+        objects: ["Sku", "Product"],
+        secretKey: process.env.GATSBY_STRIPE_SECRET_KEY,
+        downloadFiles: true,
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -18,16 +30,15 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Cici Creations`,
+        short_name: `Cici`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#000000`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/cici.svg`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-stripe`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
